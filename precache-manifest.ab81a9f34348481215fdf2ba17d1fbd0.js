9168822eeb26b97cb1d496df64ec0a0f63f0a011
jonathan1531 <jonathan.find@gmail.com>
2020-06-08 09:52:37 -0500

    Updates

---
+++ precache-manifest.ab81a9f34348481215fdf2ba17d1fbd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287816887ee27ddae7f9e71a5669fd8c",
+    "url": "/latina/index.html"
+  },
+  {
+    "revision": "a1334a9d6c21c246cb81",
+    "url": "/latina/static/css/main.25051f32.chunk.css"
+  },
+  {
+    "revision": "8cb9850890eb7a24d97b",
+    "url": "/latina/static/js/2.6e24f070.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/latina/static/js/2.6e24f070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b86026bc7aad7d7819",
+    "url": "/latina/static/js/3.76ba3496.chunk.js"
+  },
+  {
+    "revision": "a1334a9d6c21c246cb81",
+    "url": "/latina/static/js/main.0e664ab5.chunk.js"
+  },
+  {
+    "revision": "98a6aaeedce47cb46cac",
+    "url": "/latina/static/js/runtime-main.1f4ca2b2.js"
+  }
+]);
